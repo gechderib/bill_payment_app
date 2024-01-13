8c63dd0a50e400e3809e1b18719142d44ef44213
gechderib <gechderib@gmail.com>
2024-01-13 08:12:10 +0300

    helper function

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -6,7 +6,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("hello"),
+      body: Text("Splash screen"),
     );
   }
 }
